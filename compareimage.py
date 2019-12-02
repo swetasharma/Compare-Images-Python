140ffb476b0e304608eba2bcd2d4f8109c8043db
Sweta Sharma <sweta581990@gmail.com>
2019-12-02 15:32:42 +0530

    script to check if N pair of images are different

---
+++ compareimage.py
@@ -0,0 +1,48 @@
+import math, operator
+from PIL import Image
+from pathlib import Path
+# importing csv module
+import csv
+
+# user_input = raw_input("Enter the path of your file: ")
+# csv file name 
+inputfilename = Path("U:/Loblaw/records.csv")# inputfilename = Path(user_input)
+
+# name of csv file 
+outputfilename = Path("U:/Loblaw/theresult.csv")
+
+def compare(image1, image2):
+	h1 = Image.open("image1").histogram()
+	h2 = Image.open("image2").histogram()
+
+	rms = math.sqrt(reduce(operator.add,
+						   map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
+	return rms
+	
+# Creating an empty Dictionary 
+dictObj = {} 
+
+try:
+	with open(inputfilename, 'r') as csvfile:
+		# creating a csv reader object 
+		reader = csv.DictReader(csvfile)
+		# get total number of rows
+		columns = len(row)
+		print(columns)
+		if columns != 2:
+			print("There must be exactly 2 number of columns with column name as image1 and image2")
+		#for row in reader:
+    #  print(row)
+      # Create a new dictionary - data rows as dictionary objects
+      dictObj = {'image1':rows[0], 'image2':rows[1], 'similar':compare(rows[0], rows[1]), 'elapsed':'0' for rows in reader}
+			
+except IOError:
+		print("File not found or path is incorrect")
+		
+# writing to csv file 	
+with open(outputfilename, "w") as csv_file:
+	writer = csv.DictWriter(csv_file, fieldnames = ["image1", "image2", "similar", "elapsed"])
+	# writing headers (field names) 
+	writer.writeheader()
+	# writing data rows 
+	writer.writerows(dictObj)

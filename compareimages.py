eb07f8297460bdd5d4936c5ee0ec8375db10cf2d
Sweta Sharma <sweta581990@gmail.com>
2019-12-03 12:43:42 +0530

    Fixed Indentation Error.

--- compareimages.py
+++ compareimages.py
@@ -1,48 +1,49 @@
 import math, operator
-from PIL import Image
 from pathlib import Path
 # importing csv module
 import csv
 
-
-# csv file name
+# list of images to be compared csv file name
 inputfilename = Path("Images_To_Be_Compared.csv")
 
-# name of csv file 
+# Compared images name of csv file
 outputfilename = Path("Compared_Images.csv")
 
 def compare(image1, image2):
 	h1 = Image.open("image1").histogram()
 	h2 = Image.open("image2").histogram()
 
-	rms = math.sqrt(reduce(operator.add,
-			map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
-	return rms
-	
-# Creating an empty Dictionary 
-dictObj = {} 
+	return math.sqrt(reduce(operator.add, map(lambda a, b: (a - b) ** 2, h1, h2)) / len(h1))
+
+# Creating an empty Dictionary
+dictObj = {}
 
 try:
-	with open(inputfilename, 'r') as csvfile:
-		# creating a csv reader object 
+	with open(inputfilename, 'r') as csvfile: 
+		# creating a csv reader object
 		reader = csv.DictReader(csvfile)
-		# get total number of rows
+		# get total number of columns
 		columns = len(row)
 		print(columns)
 		if columns != 2:
 			print("There must be exactly 2 number of columns with column name as image1 and image2")
-		#for row in reader:
-    #  print(row)
-      # Create a new dictionary - data rows as dictionary objects
-      dictObj = {'image1':rows[0], 'image2':rows[1], 'similar':compare(rows[0], rows[1]), 'elapsed':'0' for rows in reader}
-			
+			for row in reader:
+				print(row)
+			# Create a new dictionary - data rows as dictionary objects
+			dictObj = {
+			  'image1': rows[0],
+			  'image2': rows[1],
+			  'similar': compare(rows[0], rows[1]),
+			  'elapsed': '0'
+			  for rows in reader
+			}
+
 except IOError:
-		print("File not found or path is incorrect")
-		
-# writing to csv file 	
+  print("File not found or path is incorrect")
+
+# writing to csv file
 with open(outputfilename, "w") as csv_file:
-	writer = csv.DictWriter(csv_file, fieldnames = ["image1", "image2", "similar", "elapsed"])
-	# writing headers (field names) 
+	writer = csv.DictWriter(csv_file, fieldnames = ["image1", "image2", "similar", "elapsed"])# writing headers(field names)
 	writer.writeheader()
-	# writing data rows 
+	# writing data rows
 	writer.writerows(dictObj)
